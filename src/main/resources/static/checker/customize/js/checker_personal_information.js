01cb621ee4ca8598f018b60e4f2744a017740d6e
nitmali <nitmali@126.com>
2018-04-17 21:23:52 +0800

    Fix some bug.

--- src/main/resources/static/checker/customize/js/checker_personal_information.js
+++ src/main/resources/static/checker/customize/js/checker_personal_information.js
@@ -8,32 +8,46 @@ var checkerModalApp = new Vue({
     },
     methods: {
         update_information: function (checkerInfo) {
-            $.ajax({
-                url: '/api/manager/update_checkerInfo',
-                type: 'POST',
-                data: JSON.stringify(checkerModalApp.checkerInfo, null, 4),
-                contentType: "application/json",
-                dataType: "json",
-                success: function (data) {
-                    if (data.message === "update checkerInfo success") {
-                        alert("修改成功");
-                        location.reload();
-                    }
-                },
-                error: function (XMLResponse) {
-                    alert(XMLResponse.responseText);
+            if (this.checkerInfo.name !== '' && this.checkerInfo.phone !== '') {
+                if (this.verification_phone()) {
+                    $.ajax({
+                        url: '/api/manager/update_checkerInfo',
+                        type: 'POST',
+                        data: JSON.stringify(checkerModalApp.checkerInfo, null, 4),
+                        contentType: "application/json",
+                        dataType: "json",
+                        success: function (data) {
+                            if (data.message === "update checkerInfo success") {
+                                alert("修改成功");
+                                location.reload();
+                            }
+                        },
+                        error: function (XMLResponse) {
+                            alert(XMLResponse.responseText);
+                        }
+                    });
                 }
-            });
+            }else {
+                alert("请填写完整信息！");
+            }
         },
-        reset_data:function () {
+        reset_data: function () {
             this.get_information();
         },
-        get_information:function () {
+        get_information: function () {
             $.get("/api/checker/get_checkerInfo",
                 {},
                 function (checkerInfo) {
                     checkerModalApp.checkerInfo = checkerInfo;
                 });
+        },
+        verification_phone: function () {
+            if (this.checkerInfo.phone.length !== 11 && this.checkerInfo.phone.length !== 6
+                && this.checkerInfo.phone !== '') {
+                alert("请填写有效手机号码");
+            } else {
+                return true;
+            }
         }
     }
 });
